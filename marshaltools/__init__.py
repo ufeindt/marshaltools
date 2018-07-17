02fbd9f97076ff90a4c0818bea1982563a58b6ac
R. Biswas <rbiswas4@gmail.com>
2018-07-17 16:53:29 +0200

    Added __init__.py from before, demo notebook
    -  Added __all__ to keep imports
    	new file:   ../examples/Demo_MarshalTools.ipynb
    	new file:   __init__.py
    	modified:   marshaltools.py

---
+++ marshaltools/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+import os
+from .version import __VERSION__ as __version__
+from .marshaltools import *
+here = __file__
+# basedir = os.path.split(here)[0]
+# example_data = os.path.join(basedir, 'example_data')
