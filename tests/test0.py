c6642a62d47f3a19c5011a61e09507306fb5721a
MatteoGiomi <matteo.giomi@desy.de>
2019-05-06 15:06:48 +0200

    get saved sources for time range

--- tests/test0.py
+++ tests/test0.py
@@ -1,2 +1,8 @@
 import marshaltools
+
 prog = marshaltools.ProgramList('Cosmology')
+
+
+# try load sources just for a time range
+prog = marshaltools.ProgramList('Cosmology', load_sources=False)
+prog.get_saved_sources(trange=('2009-05-18 10:04:09', '2019-05-06 10:04:09'))
